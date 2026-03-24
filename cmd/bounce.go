f7d618168f9cfbff1427aea88d86f5909481a797
Kyle Wilson <wilson15701@gmail.com>
2026-03-24 12:31:50 -0400

    add support for disabling html page, small logging updates

--- cmd/bounce.go
+++ cmd/bounce.go
@@ -53,7 +53,7 @@ var bounceCmd = &cobra.Command{
 				}
 
 				if *d.Type == "ban" {
-					logger.Info("not allowed", "ip", ip, "type", *d.Type)
+					logger.Warn("not allowed", "ip", ip, "type", *d.Type)
 					continue
 				}
 			}
